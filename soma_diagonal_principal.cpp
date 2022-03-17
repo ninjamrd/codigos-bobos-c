1ed26f2ee46529f8747045bd5752735eb2892895
ninjamrd <90119765+ninjamrd@users.noreply.github.com>
2022-03-17 20:27:58 -0300

    Update soma_diagonal_principal.cpp

--- soma_diagonal_principal.cpp
+++ soma_diagonal_principal.cpp
@@ -16,6 +16,14 @@ int main(){
 		s+=x[i][i];
 	}
 	
+	for(i=0;i<4;i++){
+		printf("|");
+		for(j=0;j<4;j++){
+			printf("%4d",x[i][j]);
+		}
+		printf("|\n");
+	}
+	
 	printf("a soma da diagonal principal eh %d",s);
 	
 }
